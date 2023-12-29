cd30a4f757ba6355a304d0f563a6e49f746de6b9
Varun Gupta <varun.gupta1690@gmail.com>
2023-12-29 20:59:23 +0530

    Breaking monolith to microsservices

---
+++ udagram-api-user/src/sequelize.ts
@@ -0,0 +1,19 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+
+  'dialect': config.dialect,
+  'dialectOptions': {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false, // This line will fix new error
+    }
+  },
+  'storage': ':memory:',
+});
