af2366c04d20bde9255d6396cd60effe5448e965
Varun Gupta <varun.gupta1690@gmail.com>
2023-12-31 08:28:17 +0530

    added config files and updated public url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac54957c5dec44c7fa4b6501477f11d4-1182981402.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
